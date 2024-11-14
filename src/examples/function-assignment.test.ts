d0ef63095b448b35ebaf954d639f7aa941d4de28
Nicholas Davidson <nicholas.davidson@daveramsey.com>
2024-11-14 14:41:37 -0600

    formatted

--- src/examples/function-assignment.test.ts
+++ src/examples/function-assignment.test.ts
@@ -1,72 +1,78 @@
-import { expect, test } from 'vitest'
-import { celsiusToFahrenheit, fahrenheitToCelsius, isDivisibleBy2And4, minutesToSeconds, oddOrEven, sum } from './function-assignments';
+import { expect, test } from "vitest";
+import {
+  celsiusToFahrenheit,
+  fahrenheitToCelsius,
+  isDivisibleBy2And4,
+  minutesToSeconds,
+  oddOrEven,
+  sum,
+} from "./function-assignments";
 
-test('adds 1 + 2 to equal 3', () => {
-    // Arrange
-   const a = 1;
-   const b = 2;
+test("adds 1 + 2 to equal 3", () => {
+  // Arrange
+  const a = 1;
+  const b = 2;
 
-    // Act
-    const result = sum(a, b);
+  // Act
+  const result = sum(a, b);
 
-    // Assert
-    expect(result).toBe(3);
-})
+  // Assert
+  expect(result).toBe(3);
+});
 
-test('converts 3 minutes to 180 seconds', () => {
-    // Arrange
-    const minutes = 3;
+test("converts 3 minutes to 180 seconds", () => {
+  // Arrange
+  const minutes = 3;
 
+  // Act
+  const result = minutesToSeconds(minutes);
 
-    // Act
-    const result = minutesToSeconds(minutes);
+  // Assert
+  expect(result).toBe(180);
+});
 
-    // Assert
-    expect(result).toBe(180);
-})
+test("converts 0 celcius to 32 fahrenheit", () => {
+  // Arrange
+  const celcius = 0;
 
-test('converts 0 celcius to 32 fahrenheit', () => {
-    // Arrange
-    const celcius = 0;
+  // Act
+  const result = celsiusToFahrenheit(celcius);
 
-    // Act
-    const result = celsiusToFahrenheit(celcius);
+  // Assert
+  expect(result).toBe(32);
+});
 
-    // Assert
-    expect(result).toBe(32);
-})
+test("converts 32 fahrenheit to 0 celsius", () => {
+  // Arrange
+  const fahrenheit = 32;
 
-test('converts 32 fahrenheit to 0 celsius', () => {
-    // Arrange
-    const fahrenheit = 32;
+  // Act
+  const result = fahrenheitToCelsius(fahrenheit);
 
-    // Act
-    const result = fahrenheitToCelsius(fahrenheit);
+  // Assert
+  expect(result).toBe(0);
+});
 
-    // Assert
-    expect(result).toBe(0);
-})
+test("number is divisible by 4 & 2", () => {
+  // Arrange
+  const num1 = 4;
+  const num2 = 8;
 
-test('number is divisible by 4 & 2', () => {
-    // Arrange
-    const num1 = 4;
-    const num2 = 8;
+  // Act
+  const result = isDivisibleBy2And4(num1, num2);
 
-    // Act
-    const result = isDivisibleBy2And4(num1, num2);
+  // Assert
+  expect(result).toBe(true);
+});
 
-    // Assert
-    expect(result).toBe(true);
-})
+test("tests if number is odd or even", () => {
+  // Arrange
+  const num = 4;
+  const even = num % 2 === 0;
 
-test('tests if number is odd or even', () => {
-    // Arrange
-    const num = 4;
-    const even = num % 2 === 0;
+  // Act
+  const result = oddOrEven(num);
 
-    // Act
-    const result = oddOrEven(num);
-    
-    // Assert
-    expect(result).toBe("even");
-})
+  // Assert
+  expect(result).toBe("even");
+});
