68feba5411313114968b6fe6ad60ee40ebcad713
Nicholas Davidson <nicholas.davidson@daveramsey.com>
2024-12-27 09:06:48 -0600

    added learnings.md file and completed part2

--- advent-of-code/solutions/2022/01/part2.ts
+++ advent-of-code/solutions/2022/01/part2.ts
@@ -1,5 +1,37 @@
 console.log("\nHello - Advent of Code Day 1!\n");
 
-const input = await Bun.file('solutions/2022/01/input.txt').text();
-console.log("Unsorted Input:");
-console.log(input);
+const input = await Bun.file(`${import.meta.dir}/input.txt`).text();
+
+const elves = parseInput(input);
+const elfSums = elves.map(sumNumbers);
+const elfSumsSorted = elfSums.sort(compareNumbers);
+const topThreeElves = elfSumsSorted.slice(0, 3);
+const topThreeTotal = sumNumbers(topThreeElves);
+
+console.log({ elfSums, elfSumsSorted, topThreeElves, topThreeTotal });
+
+function parseInput(rawInput: string): number[][] {
+  const rawElfStrings = rawInput.split("\n\n");
+  const parsedElves = rawElfStrings.map(convertRawElfStringToNumbers);
+  return parsedElves;
+}
+
+function convertRawElfStringToNumbers(rawElfString: string): number[] {
+  const elfStrings = rawElfString.split("\n");
+  return elfStrings.map(Number);
+}
+
+function sumNumbers(numbers: number[]): number {
+  let addedNumbers = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    addedNumbers = numbers[i] + addedNumbers;
+  }
+
+  return addedNumbers;
+}
+
+function compareNumbers(a: number, b: number) {
+  if (a > b) {
+    return -1;
+  } else return 1;
+}
