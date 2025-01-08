69f644fd6e084160adbadd6a78e46090a6d05643
Nicholas Davidson <nicholas.davidson@daveramsey.com>
2025-01-08 15:17:46 -0600

    added to code

--- advent-of-code/solutions/2015/02/part1.ts
+++ advent-of-code/solutions/2015/02/part1.ts
@@ -5,12 +5,50 @@ console.log("Unsorted Input:");
 console.log(input, "\n");
 
 const boxDimensionStrings = input.trim().split("\n");
-const boxDimensions = boxDimensionStrings.map(parseBoxDimensions);
-console.log({ boxDimensions });
+const boxDimensionsList = boxDimensionStrings.map(parseBoxDimensions);
+const wrappingPaperNeededForEachBox = boxDimensionsList.map(
+  calcualateTotalPaperForBox
+);
+const totalSquareFeetOfWrappingPaper = addNumbers(wrappingPaperNeededForEachBox);
+
+console.log({ boxDimensionsList, wrappingPaperNeededForEachBox, totalSquareFeetOfWrappingPaper });
 
 function parseBoxDimensions(dimensionsString: string): number[] {
   const boxDimensions = dimensionsString.split("x");
   return boxDimensions.map(Number);
 }
 
-console.log({ boxDimensionStrings });
+function calcualateTotalPaperForBox(boxDimensions: number[]): number {
+  return (
+    calculateSurfaceAreaOfBox(boxDimensions) +
+    calculateAreaOfSmallestSide(boxDimensions)
+  );
+}
+
+function calculateSurfaceAreaOfBox(boxDimensions: number[]): number {
+  const length = boxDimensions[0];
+  const width = boxDimensions[1];
+  const height = boxDimensions[2];
+  const surfaceArea =
+    2 * length * width + 2 * width * height + 2 * height * length;
+
+  return surfaceArea;
+}
+
+function calculateAreaOfSmallestSide(boxDimensions: number[]): number {
+  const sortBoxDimensionsSmalestLargest = boxDimensions.sort(
+    (a: number, b: number) => a - b
+  );
+  const smallestSideArea =
+    sortBoxDimensionsSmalestLargest[0] * sortBoxDimensionsSmalestLargest[1];
+
+  return smallestSideArea;
+}
+
+function addNumbers(numbers: number[]): number {
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total = total + numbers[i];
+  }
+  return total;
+}
